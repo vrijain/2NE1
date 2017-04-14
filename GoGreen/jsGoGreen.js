024fe2aacd246da3d470fd500949795e86eae471
vrijain <vri3jain@gmail.com>
2017-04-14 15:27:45 +0530

    Add files via upload

---
+++ GoGreen/jsGoGreen.js
@@ -0,0 +1,17 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+}
+var x = document.getElementById("demo");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
